9ce9f5fb6284b3149504fdf0442ffd69347e2001
Peter Korduan <peter.korduan@gdi-service.de>
2019-01-16 19:55:06 +0100

    Add noseRemove after cleanpolygon and close only gap < gap_area_tolerance and add usage msg in scripts

--- functions/n_gdi_cleanPolygon.sql
+++ functions/n_gdi_cleanPolygon.sql
@@ -1,42 +1,48 @@
---DROP function gdi_cleanPolygon(geometry, integer, double precision, double precision)
---DROP function gdi_cleanPolygon(geometry, integer, double precision, double precision)
+--DROP function gdi_cleanPolygon(geometry, integer, double precision, double precision);
 CREATE OR REPLACE FUNCTION public.gdi_cleanPolygon(
   geom geometry,
   epsg_code integer,
   distance_tolerance double precision,
   area_tolerance double precision
 )
-RETURNS SETOF record AS
+RETURNS SETOF geometry AS
 $$
   SELECT
-    ST_GeometryN(
-      geom,
-      generate_series(
-        1,
-        ST_NumGeometries(geom)
-      )
-    ) AS geom
+    *
   FROM
     (
       SELECT
-        gdi_FilterRings(
-          gdi_FingerCut(
-            ST_CollectionExtract(
-              ST_MakeValid(
-                 ST_SimplifyPreserveTopology(
-                  ST_Transform(
-                    $1,
-                    $2
-                  ),
-                  $3
-                )
-              ),
-              3
-            ),
-            $3
-          ),
-          $4
+        ST_GeometryN(
+          geom,
+          generate_series(
+            1,
+            ST_NumGeometries(geom)
+          )
         ) AS geom
-    ) foo
+      FROM
+        (
+          SELECT
+            gdi_FilterRings(
+              gdi_FingerCut(
+                ST_CollectionExtract(
+                  ST_MakeValid(
+                     ST_SimplifyPreserveTopology(
+                      ST_Transform(
+                        $1,
+                        $2
+                      ),
+                      $3
+                    )
+                  ),
+                  3
+                ),
+                $3
+              ),
+              $4
+            ) AS geom
+        ) foo
+    ) bar
+  WHERE
+    ST_Area(geom) > $4
 $$
 LANGUAGE sql STABLE;
